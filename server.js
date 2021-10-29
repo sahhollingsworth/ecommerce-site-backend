607ac0661abb8d2f2cc1969495dffd6a08f71c60
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-10-28 21:50:03 -0700

    Fix: Added sequelize import to server.js and updated connection.js to process.env.DB_PASSWORD to match value in .env file.
    Couldn't add seed data before these changes, so this resolved error node:85780.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+//Import Express package
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
